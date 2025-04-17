6ac3eb7b784fe369be696831d54b65896e27743f
hyk <hyk339@gmail.com>
2025-04-17 23:05:15 +0900

    Series 실습

---
+++ Series실습.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+dict_data = {'a':1, 'b':2, 'c':3}
+series = pd.Series(dict_data)
+
+# print(series)
+# print(series.index)
+# print(series.values)
+
+list_data = ['a', 'b', 'c']
+series_2 = pd.Series(list_data)
+
+print(series_2)
